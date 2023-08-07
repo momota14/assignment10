7c646ff053a9e6d24c3ada16005ac287900aea68
Momota khatun <137165400+momota14@users.noreply.github.com>
2023-08-07 13:13:07 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,2 @@
 export default function Home() {
-  return (
-    <div className="text-2xl mt-9"> This is home page</div>
-  )}
+return <div className="pt-10 px-5 text-2xl"> This is home page</div>}
